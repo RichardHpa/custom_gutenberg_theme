e148bdfb2277675e5da4585118de2912e152fbf6
Richard Hpa <richard.hpa@hotmail.com>
2018-12-14 08:19:50 +1300

    edited theme

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
 <?php get_header(); ?>
-
-<div class="container">
-    <div class="row">
-        <?php if(have_posts()): ?>
-            <?php while(have_posts()): the_post();?>
+<?php if(have_posts()): ?>
+    <?php while(have_posts()): the_post();?>
+        <div class="container">
+            <div class="row">
                 <div class="col-12">
                     <h1><?php the_title(); ?></h1>
                 </div>
-
-                <div class="col-12">
-                    <?php the_content(); ?>
-                </div>
-            <?php endwhile; ?>
-        <?php endif; ?>
-    </div>
-</div>
-
+            </div>
+        </div>
+        <div class="">
+            <?php the_content(); ?>
+        </div>
+    <?php endwhile; ?>
+<?php endif; ?>
 
 <?php get_footer(); ?>
